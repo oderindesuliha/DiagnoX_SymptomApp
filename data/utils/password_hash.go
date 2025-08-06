38956ef34d44d44f8c4704d784bb4d4ade50447d
Oderinde Suliha <oderindesuliha@gmail.com>
2025-08-06 16:18:24 +0100

    symptom app

---
+++ data/utils/password_hash.go
@@ -0,0 +1,9 @@
+//package utils
+//
+//import "golang.org/x/crypto/bcrypt"
+//
+//func checkPassword(password hash String) bool {
+//	err := bcrypt.CompareHashAndPassword([]byte(password), []byte(password))
+//	return err == nil
+//
+//}
